a2d7c3909a411973e9c94b63fe45619d6e2908aa
Carsten Ullrich <carsten.ullrich@macros-group.com>
2026-03-25 14:20:21 +0100

    GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   output: 'export',
+  basePath: isProd ? '/stahnsdorf' : '',
+  assetPrefix: isProd ? '/stahnsdorf/' : '',
 }
 module.exports = nextConfig
